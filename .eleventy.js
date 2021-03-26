f43e340bbb303490384c3bb2c4f6a7eef187a652
Bryn Masvaure <masvaurebryn@gmail.com>
2021-03-26 21:38:49 +0200

    adding netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/css")
     eleventyConfig.addPassthroughCopy("src/images/uploads")
+    eleventyConfig.addPassthroughCopy("src/admin")
     eleventyConfig.addCollection("posts", function(collection) {
         return collection.getFilteredByGlob("src/posts/*.md");
     });
