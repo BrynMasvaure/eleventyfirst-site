e0800933a1efcd3887b2b79b7144a952eb475eee
Bryn Masvaure <masvaurebryn@gmail.com>
2021-03-28 13:14:29 +0200

    eleventy.js update

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets/css")
+    eleventyConfig.addPassthroughCopy("src/assets/css")
     eleventyConfig.addPassthroughCopy("src/images/uploads")
     eleventyConfig.addPassthroughCopy("src/admin")
     eleventyConfig.addCollection("posts", function(collection) {
